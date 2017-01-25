5a6f9c5c7ba4bb99e42538e45da0a8ee73ef0295
Suyeol Jeon <devxoul@gmail.com>
2017-01-25 21:33:15 +0900

    Write test for emptiness assertion

---
+++ Tests/RxAssertionEmptinessTests.swift
@@ -0,0 +1,32 @@
+//
+//  RxAssertionEmptinessTests.swift
+//  RxExpect
+//
+//  Created by Suyeol Jeon on 25/01/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+import RxTest
+import RxExpect
+
+final class RxAssertionEmptinessTests: XCTestCase {
+
+  func testAssertEmpty() {
+    RxExpect("it should assert emptiness") { test in
+      let source = PublishSubject<Int>()
+      test.input(source, [])
+      test.assert(source).isEmpty()
+    }
+  }
+
+  func testAssertNotEmpty() {
+    RxExpect("it should assert non-emptiness") { test in
+      let source = PublishSubject<Int>()
+      test.input(source, [next(100, 1)])
+      test.assert(source).isNotEmpty()
+    }
+  }
+
+}
