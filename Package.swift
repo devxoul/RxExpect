26984cade85cb84f4062d90837200eb4fe845e42
Suyeol Jeon <devxoul@gmail.com>
2017-04-27 03:22:09 +0900

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+  name: "RxExpect",
+  dependencies: [
+    // temporary url for RxTest support
+    .Package(url: "https://github.com/devxoul/RxSwift.git", Version(3, 4, 1)),
+  ]
+)
