97699ecd0ba74dec0f0dd1b455bf7605d12ca146
윤중현 <tokijh@naver.com>
2021-01-04 15:14:29 +0900

    Update RxSwift to 6.0.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     .library(name: "RxExpect", targets: ["RxExpect"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
   ],
   targets: [
     .target(name: "RxExpect", dependencies: ["RxSwift", "RxTest"]),
